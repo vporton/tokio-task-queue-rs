83690d1d813d1d7e41b3412a059710b71631c3f0
Victor Porton <porton.victor@gmail.com>
2022-02-11 01:24:22 +0200

    it seems that it works

--- src/bin/mytest.rs
+++ src/bin/mytest.rs
@@ -8,9 +8,9 @@ async fn main() {
     env_logger::init();
 
     let queue = Arc::new(Mutex::new(TaskQueue::new()));
-    // TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 1 + 1; })).await;
+    TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 1 + 1; })).await;
     let join_handle = TaskQueue::spawn(queue.clone());
-    // TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 2 + 2; })).await;
-    queue.lock().await.notify(); // Stop the scheduler.
+    TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 2 + 2; })).await;
+    queue.lock().await.notify().await; // Stop the scheduler.
     join_handle.await.unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::collections::VecDeque;
-use log::debug;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
@@ -10,27 +9,25 @@ use tokio::task::JoinHandle;
 pub struct TaskQueue {
     // join_handle: JoinHandle<()>,
     queued_tasks: VecDeque<Pin<Box<dyn Future<Output = ()> + Send>>>,
-    notify: Notify, // Used for both notifications after a task queued and for closing the thread.
+    notify: Arc<Mutex<Notify>>, // Used for both notifications after a task queued and for closing the thread.
 }
 
 impl TaskQueue {
     pub fn new() -> Self {
         Self {
             queued_tasks: VecDeque::new(),
-            notify: Notify::new(),
+            notify: Arc::new(Mutex::new(Notify::new())),
         }
     }
     async fn _task(this: Arc<Mutex<Self>>) { // FIXME: Check everything
-        let notify = &this.lock().await.notify;
-        // debug!("guard.queued_tasks.len() = {}", guard.queued_tasks.len());
-        debug!("TASK");
+        let notify = this.lock().await.notify.clone();
         let front = this.lock().await.queued_tasks.pop_front();
-        debug!("TASK2");
         if let Some(front) = front {
             // Not all `push_task` notifications handled, we will return on a subsequent loop iteration.
             // So, no notification is lost.
+            let notify_guard = notify.lock().await;
             select! {
-                _ = notify.notified() => {
+                _ = notify_guard.notified() => {
                     // All notifications by `push_task` are already handled (otherwise, it wouldn't be empty),
                     // so, it is a notification to interrupt.
                     return;
@@ -38,22 +35,19 @@ impl TaskQueue {
                 _ = front => { }
             };
         } else {
-            notify.notified().await
+            notify.lock().await.notified().await
         }
-
-        debug!("UUU");
     }
     pub async fn push_task(this: Arc<Mutex<Self>>, task: Pin<Box<dyn Future<Output=()> + Send>>) {
         this.lock().await.queued_tasks.push_back(task);
-        this.lock().await.notify.notify_one(); // notify_waiters() doesn't work here, because it would need already to wait.
+        this.lock().await.notify.lock().await.notify_one(); // notify_waiters() doesn't work here, because it would need already to wait.
     }
     pub fn spawn(this: Arc<Mutex<Self>>) -> JoinHandle<()> {
-        debug!("RRR");
         spawn(Self::_task(this))
     }
     /// If notified when task queue is empty, stops the scheduler.
-    pub fn notify(&self) {
-        self.notify.notify_one();
+    pub async fn notify(&self) {
+        self.notify.lock().await.notify_one();
     }
     // pub fn notifier(&self) -> &Notify { // I don't expose it to public API because of mess in notify_waiters()
     //     &self.notify
@@ -72,11 +66,11 @@ mod tests {
         let rt  = Runtime::new().unwrap();
         rt.block_on(async {
             let queue = Arc::new(Mutex::new(TaskQueue::new()));
-            // TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 1 + 1; })).await;
+            TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 1 + 1; })).await;
             let join_handle = TaskQueue::spawn(queue.clone());
-            // TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 2 + 2; })).await;
-            queue.lock().await.notify(); // Stop the scheduler.
-            // join_handle.await.unwrap();
+            TaskQueue::push_task(queue.clone(), Box::pin(async { let _ = 2 + 2; })).await;
+            queue.lock().await.notify().await; // Stop the scheduler.
+            join_handle.await.unwrap();
         });
     }
 }
