d919e55acb939ee9e5766b00164c37a2751da4ad
Victor Porton <porton.victor@gmail.com>
2022-02-11 01:40:10 +0200

    attempt to rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,28 @@ use std::pin::Pin;
 use std::sync::Arc;
 use tokio::{select, spawn};
 use tokio::sync::{Mutex, Notify};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::task::JoinHandle;
 
+// FIXME: To overcome the race condition, use a tx/rx pair instead of deque.
 pub struct TaskQueue {
     // join_handle: JoinHandle<()>,
-    queued_tasks: VecDeque<Pin<Box<dyn Future<Output = ()> + Send>>>,
-    notify: Arc<Mutex<Notify>>, // Used for both notifications after a task queued and for closing the thread.
+    queued_tasks_tx: UnboundedSender<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    queued_tasks_rx: UnboundedReceiver<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    notify_interrupt: Arc<Mutex<Notify>>, // Used for both notifications after a task queued and for closing the thread.
 }
 
 impl TaskQueue {
     pub fn new() -> Self {
+        let (queued_tasks_tx, queued_tasks_rx) =
         Self {
-            queued_tasks: VecDeque::new(),
-            notify: Arc::new(Mutex::new(Notify::new())),
+            queued_tasks_tx,
+            queued_tasks_rx,
+            notify_interrupt: Arc::new(Mutex::new(Notify::new())),
         }
     }
     async fn _task(this: Arc<Mutex<Self>>) { // FIXME: Check everything
-        let notify = this.lock().await.notify.clone();
+        let notify = this.lock().await.notify_interrupt.clone();
         loop {
             let front = this.lock().await.queued_tasks.pop_front();
             if let Some(front) = front {
@@ -43,14 +48,14 @@ impl TaskQueue {
     }
     pub async fn push_task(this: Arc<Mutex<Self>>, task: Pin<Box<dyn Future<Output=()> + Send>>) {
         this.lock().await.queued_tasks.push_back(task);
-        this.lock().await.notify.lock().await.notify_one(); // notify_waiters() doesn't work here, because it would need already to wait.
+        this.lock().await.notify_interrupt.lock().await.notify_one(); // notify_waiters() doesn't work here, because it would need already to wait.
     }
     pub fn spawn(this: Arc<Mutex<Self>>) -> JoinHandle<()> {
         spawn(Self::_task(this))
     }
     /// If notified when task queue is empty, stops the scheduler.
     pub async fn notify(&self) {
-        self.notify.lock().await.notify_one();
+        self.notify_interrupt.lock().await.notify_one();
     }
     // pub fn notifier(&self) -> &Notify { // I don't expose it to public API because of mess in notify_waiters()
     //     &self.notify
