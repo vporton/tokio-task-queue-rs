ee9ad0b167d0fcc622dc730f006f24ad32653cd4
Victor Porton <porton.victor@gmail.com>
2022-02-11 21:23:48 +0200

    misc

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,73 @@
-use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
+use futures::future::ready;
+use futures::stream::Stream;
+use futures::StreamExt;
 use tokio::{select, spawn};
-use tokio::sync::{Mutex, Notify};
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
 
-// FIXME: To overcome the race condition, use a tx/rx pair instead of deque.
-pub struct TaskQueue {
-    // join_handle: JoinHandle<()>,
-    queued_tasks_tx: UnboundedSender<Pin<Box<dyn Future<Output = ()> + Send>>>,
-    queued_tasks_rx: UnboundedReceiver<Pin<Box<dyn Future<Output = ()> + Send>>>,
-    notify_interrupt: Arc<Mutex<Notify>>, // Used for both notifications after a task queued and for closing the thread.
+struct TaskItem {
+    task: Pin<Box<dyn Future<Output = ()> + Send + Unpin>>,
+    interrupt_previous: bool,
 }
 
-impl TaskQueue {
-    pub fn new() -> Self {
-        let (queued_tasks_tx, queued_tasks_rx) =
+// FIXME: To overcome the race condition, use a tx/rx pair instead of deque.
+pub struct TaskQueue<TaskStream: Stream<Item = TaskItem> + Send + 'static>
+{
+    // join_handle: JoinHandle<()>,
+    task_stream: Pin<Box<TaskStream>>,
+    current_task: Option<Pin<Box<dyn Future<Output = ()> + Send + Unpin>>>,
+}
+
+impl<TaskStream: Stream<Item = TaskItem> + Send + 'static> TaskQueue<TaskStream>
+{
+    pub fn new(task_stream: TaskStream) -> Self {
         Self {
-            queued_tasks_tx,
-            queued_tasks_rx,
-            notify_interrupt: Arc::new(Mutex::new(Notify::new())),
+            current_task: None,
+            task_stream: Box::pin(task_stream),
         }
     }
     async fn _task(this: Arc<Mutex<Self>>) { // FIXME: Check everything
-        let notify = this.lock().await.notify_interrupt.clone();
         loop {
-            let front = this.lock().await.queued_tasks.pop_front();
-            if let Some(front) = front {
-                // Not all notifications from `push_task` are already handled, we will return on a subsequent loop iteration.
-                // So, no notification is lost.
-                let notify_guard = notify.lock().await;
-                select! {
-                    _ = notify_guard.notified() => {
-                        // All notifications by `push_task` are already handled (otherwise, it wouldn't be empty),
-                        // so, it is a notification to interrupt.
+            let front_future = this.lock().await.task_stream.next();
+            select! {
+                _ = if let Some(current) = this.lock().await.current_task {
+                    current as Pin<Box<dyn Future<Output = ()> + Send + Unpin>>
+                } else {
+                    Box::pin(ready(())) as Pin<Box<dyn Future<Output = ()> + Send + Unpin>>
+                } => { }
+                front = front_future => {
+                    if let Some(front) = front {
+                        if !front.interrupt_previous {
+                            if let Some(current) = this.lock().await.current_task {
+                                current.await;
+                            }
+                        }
+                        this.lock().await.current_task = Some(front.task);
+                        this.lock().await.current_task.unwrap().await;
+                    } else {
+                        if let Some(current) = this.lock().await.current_task {
+                            current.await;
+                        }
                         return;
                     }
-                    _ = front => { }
                 }
-                ;
-            } else {
-                notify.lock().await.notified().await
-            }
+            };
         }
     }
-    pub async fn push_task(this: Arc<Mutex<Self>>, task: Pin<Box<dyn Future<Output=()> + Send>>) {
-        this.lock().await.queued_tasks.push_back(task);
-        this.lock().await.notify_interrupt.lock().await.notify_one(); // notify_waiters() doesn't work here, because it would need already to wait.
-    }
+    // pub async fn push_task(this: Arc<Mutex<Self>>, task: Pin<Box<dyn Future<Output=()> + Send>>) {
+    //     this.lock().await.queued_tasks.push_back(task);
+    //     this.lock().await.notify_interrupt.lock().await.notify_one(); // notify_waiters() doesn't work here, because it would need already to wait.
+    // }
     pub fn spawn(this: Arc<Mutex<Self>>) -> JoinHandle<()> {
-        spawn(Self::_task(this))
+        spawn(Self::_task(this)) // TODO: Make it interruptible.
     }
-    /// If notified when task queue is empty, stops the scheduler.
-    pub async fn notify(&self) {
-        self.notify_interrupt.lock().await.notify_one();
-    }
+    // If notified when task queue is empty, stops the scheduler.
+    // pub async fn notify(&self) {
+    //     self.notify_interrupt_queue.lock().await.notify_one();
+    // }
     // pub fn notifier(&self) -> &Notify { // I don't expose it to public API because of mess in notify_waiters()
     //     &self.notify
     // }
